9897327ee6070220325d6d83d7338bc8ee32031d
victimlisten <victimlisten@gmail.com>
2020-09-21 15:08:48 +0700

    cleaning

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import {appRouter} from "./router";
-import {db} from "./db";
 const app = express();
 app.use(express.json());
 appRouter(app);
--- src/utils/checks.ts
+++
@@ -1,2 +0,0 @@
-import check from "check-types";
-
